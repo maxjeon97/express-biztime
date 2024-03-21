f7d2fdfe1ac015bead2b7fb006a8e52b8f1ccbfa
Max Jeon <maxjeon97@gmail.com>
2024-03-21 16:24:00 -0700

    Add tests for companies

--- routes/companies.test.js
+++ routes/companies.test.js
@@ -32,6 +32,7 @@ beforeEach(async function () {
 describe("GET /companies", function () {
   test("Gets a list of companies", async function () {
     const response = await request(app).get(`/companies`);
+
     expect(response.body).toEqual({ companies: [{ code: testCompany.code, name: testCompany.name }] });
   });
 });
@@ -39,20 +40,23 @@ describe("GET /companies", function () {
 describe("GET /companies/:code", function () {
   test("Gets a company", async function () {
     const response = await request(app).get(`/companies/${testCompany.code}`);
-    expect(response.body).toEqual({ company: {
-      code: testCompany.code,
-      name: testCompany.name,
-      description: testCompany.description,
-      invoices: [testInvoice.id]
-    } });
+
+    expect(response.body).toEqual({
+      company: {
+        code: testCompany.code,
+        name: testCompany.name,
+        description: testCompany.description,
+        invoices: [testInvoice.id]
+      }
+    });
   });
 
   test("Responds with 404 if can't find company", async function () {
     const response = await request(app).get("/companies/0");
+
     expect(response.statusCode).toEqual(404);
   });
 });
-
 
 describe("POST /companies", function () {
   test("Creates a company", async function () {
@@ -61,26 +65,77 @@ describe("POST /companies", function () {
       name: "Tesla",
       description: "Electric cars"
     };
+
     const response = await request(app)
-    .post("/companies")
-    .send(addedCompany);
-    expect(response.body).toEqual({
-      company: {code: "tsl", name: "Tesla", description: "Electric cars"}
-    });
+      .post("/companies")
+      .send(addedCompany);
+
     expect(response.statusCode).toEqual(201);
-  })
+    expect(response.body).toEqual({
+      company: { code: "tsl", name: "Tesla", description: "Electric cars" }
+    });
+
+    const result = await db.query(
+      `SELECT *
+        FROM companies
+        WHERE code = 'tsl'`);
+    expect(result.rows.length).toEqual(1);
+  });
 
   test("Responds with 400 if empty body", async function () {
     const response = await request(app)
       .post(`/companies`)
       .send();
+
+    expect(response.statusCode).toEqual(400);
+  });
+});
+
+describe("PUT /companies/:code", function () {
+  test("Updates a single company", async function () {
+    const response = await request(app)
+      .put(`/companies/${testCompany.code}`)
+      .send({ name: "Test Co2", description: "Test description2" });
+
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toEqual({
+      company: { code: "tst", name: "Test Co2", description: "Test description2" }
+    });
+  });
+
+  test("Responds with 400 if empty body", async function () {
+    const response = await request(app)
+      .put(`/companies/${testCompany.code}`)
+      .send();
+
     expect(response.statusCode).toEqual(400);
   });
 
-})
+  test("Responds with 404 if can't find company", async function () {
+    const response = await request(app)
+      .put(`/companies/test`)
+      .send({ name: "Test Co2", description: "Test description2" });
 
+    expect(response.statusCode).toEqual(404);
+  });
+});
 
+describe("DELETE /companies/:code", function () {
+  test("Deletes a single company", async function () {
+    const response = await request(app)
+      .delete(`/companies/${testCompany.code}`);
 
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toEqual({ status: "deleted" });
+  });
+
+  test("Responds with 404 if can't find company", async function () {
+    const response = await request(app)
+      .delete(`/companies/0`);
+
+    expect(response.statusCode).toEqual(404);
+  });
+});
 
 
 
