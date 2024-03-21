c45d6f1ab7f23b9b97b8810502f9d7c89a1262ca
Yagiz Ulasoglu <yulasoglu@gmail.com>
2024-03-21 11:50:40 -0700

    Company routes are done

--- routes/companies.js
+++ routes/companies.js
@@ -71,4 +71,47 @@ router.post("", async function (req, res, next) {
   return res.status(201).json({ company: company });
 });
 
+/** PUT request -
+ * Given JSON body data {name, description}
+ * Update the company, returning JSON {company: {code, name, description}}
+*/
+
+router.put("/:code", async function (req, res, next) {
+  if (req.body === undefined ||
+    "code" in req.body ||
+    !("name" in req.body) ||
+    !("description" in req.body)) {
+    throw new BadRequestError("Not allowed");
+  }
+
+  const code = req.params.code;
+  const { name, description } = req.body;
+  const results = await db.query(
+    `UPDATE companies
+         SET name=$1, description=$2
+         WHERE code = $3
+         RETURNING code, name, description`,
+    [name, description, code]);
+  const company = results.rows[0];
+
+  if (!company) throw new NotFoundError("This company does not exist");
+  return res.json({ company: company });
+});
+
+
+/** Delete request -
+ * Delete the company, returning JSON { status: "deleted" }
+*/
+
+router.delete("/:code", async function (req, res, next) {
+  const code = req.params.code;
+  const results = await db.query(
+    "DELETE FROM companies WHERE code = $1 RETURNING code", [code]);
+  const company = results.rows[0];
+
+  if (!company) throw new NotFoundError("This company does not exist");
+  return res.json({ status: "deleted" });
+});
+
+
 module.exports = router;
